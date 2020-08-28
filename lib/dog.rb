a0e904031a9bf7d199600bde0edffd47545dd3ed
Adam <adamadolfo8@gmail.com>
2020-08-28 08:46:40 -0400

    not complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-  # code goes here
+  @@all_dogs = []
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all_dogs << self
+  end
+
+  def self.all
+    @@all_dogs
+  end
+  
+
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,79 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  @@all_owners = []
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all_owners.push(self)
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def self.all
+    @@all_owners
+  end
+  
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def cats
+    Cat.all.select do |kat|
+       kat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    self.cats.each do |kat|
+      kat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+  #   if self.cats.owner == nil
+  #     self.cats.mood = "nervous"
+  #   end
+  #   if self.dogs.owner == nil
+  #     self.dogs.mood = "nervous"
+  #   end
+
+  #   self.cats.each do |kat|
+  #     kat.owner = nil
+  #   end
+  #   self.dogs.each do |dog|
+  #       dog.owner = nil
+  #   end
+  #    self.reset_all
+  # end
 end
+
+
